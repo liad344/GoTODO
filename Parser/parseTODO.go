5cad36aaf1b4ffd0bc264928a1b6b55c1d5d8e74
liad344 <liad.sagi3@gmail,com>
2020-04-11 14:58:29 +0300

    Continued parser

--- Parser/parseTODO.go
+++ Parser/parseTODO.go
@@ -4,12 +4,24 @@ import (
 	log "github.com/sirupsen/logrus"
 	"io/ioutil"
 	"os"
+	"path"
 )
 
-func parse(path string) {
-	f, err := os.Open(path)
+func parse(p string) {
+	files, err := ioutil.ReadDir(p)
 	if err != nil {
 		log.Error(err)
 	}
+	for _, f := range files {
+		if f.IsDir() {
+			parse(path.Join(p, f.Name()))
+		} else {
+			parseFile(f)
+		}
+	}
+
+}
+
+func parseFile(f os.FileInfo) {
 
 }
